9859d804a12abcd78766ba32fe62fd971445dd47
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-07-14 09:33:26 -0600

    escape >

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -44,33 +44,6 @@ export default function Post({ postData }) {
             <Date dateString={postData.date} />
           </div>
           <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          ```mermaid!
-flowchart TD
-    A[user request] -.-{'>'} B(Load balancer)
-    B -.-{'>'} C(DNS Server)
-    C -.-{'>'} D[/email-rest-service.sonam.cloud/]
-
-    subgraph k8[Kubernetes Cluster]
-    subgraph ingress[Ingress]
-    F(Nginx Controller)
-    end
-
-    subgraph app[email-rest-service]
-    G(Kubernetes Service)
-    G -- uses authId header for user context --{'>'} H(email-rest-service pod)
-    H -- validate jwt token using jwt-validator --{'>'} H
-    end    
-    
-    end
-    F -. 3 calls-service .-{'>'} G
-    B -.-{'>'} k8
-    
-```
-<div class="mermaid">
-graph LR
-A--{'>'}B
-  </div>
-       
         </article>
       </Layout>
     );
